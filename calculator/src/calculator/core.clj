2de5eeddc25ef6365591cb25f69a5a7c17b4d9df
Yehonathan Sharvit <viebel@gmail.com>
2018-08-14 21:48:40 +0300

    fix typo

--- calculator/src/calculator/core.clj
+++ calculator/src/calculator/core.clj
@@ -4,6 +4,8 @@
 (defn foo [x]
   (* x 999))
 
+
+
 (defn calc "calculator"
   {:test #(do
             (is (= (calc :+ 1 2) 3)))}
@@ -25,8 +27,8 @@
 (defn currency-units [currency]
   (name currency))
 
-(defn display-currrency-conversion [amount currency-in currency-out]
-  (let [amount-out (convert-currency amount currency-in currency-out)
+(defn display-currency-conversion [amount currency-in currency-out]
+      (let [amount-out (convert-currency amount currency-in currency-out)
         currency-in-units (currency-units currency-in)
         currency-out-units (currency-units currency-out)]
     (str amount " " currency-in-units " is equivalent to: " (format "%.2f" amount-out) " " currency-out-units)))
@@ -38,8 +40,8 @@
   (is (= (convert-currency 1 :dollar :euro) (* dollar->euro 1)))
   (is (= (convert-currency 1 :euro :dollar) (* (/ 1 dollar->euro) 1))))
 
-(deftest display-currrency-conversion-test
-  (is (= (display-currrency-conversion 1 :euro :dollar) "1 euro is equivalent to: 1.16 dollar")))
+(deftest display-currency-conversion-test
+  (is (= (display-currency-conversion 1 :euro :dollar) "1 euro is equivalent to: 1.16 dollar")))
 
 
 
