41d4b23e481e2f0ddd19102f70d8fc4540134ff5
Yehonathan Sharvit <viebel@gmail.com>
2018-08-20 07:23:02 +0300

    ns :rename

--- snippets/src/snippets/core.clj
+++ snippets/src/snippets/core.clj
@@ -1,30 +1,40 @@
-(ns snippets.core)
+(ns snippets.core
+  (:require [clojure.set :refer [intersection] :rename {intersection ii}]))
+
+
+
+
+(comment
+  (ii #{1 2} #{1 4}))
+
 
 (defn foo
   "I don't do a whole lot."
-  [x]
+  [x y]
   (println x "Hello, World!"))
 
 
-(if (> x 2)
-  3 
-  (if (< x 10)
-      (+ x  20)
-      (+ x 10)))
+(comment (if (> x 2)
+           3
+           (if (< x 10)
+                (+ x  20)
+                (+ x 10)))
 
-(+ (* 2 3)
-   (* 5 7))
+         (+ (* 2 3)
+            (* 5 7)))
 
-
+(defn bar [a b] a)
 
 (foo 1 
      (bar 2 3))
 (foo 1 2)
-(+ 2 3)
-(> x 2)
-(if (> x 2)
+
+(comment
+  (+ 2 3)
+  (> x 2)
+  (if (> x 2)
     3
     5)
-(+ 2 3)
-(> x 2)
+  (+ 2 3)
+  (> x 2))
 
